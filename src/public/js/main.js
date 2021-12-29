dc15e3a4725177ffca8102ac1aa0f8628cfe592c
jcbbb <avaz28082000@gmail.com>
2021-12-29 15:24:17 +0500

    Ready. Set. Go

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,35 +1,237 @@
 import {
   Scene,
-  BoxGeometry,
-  MeshBasicMaterial,
   Mesh,
+  PlaneBufferGeometry,
+  MeshStandardMaterial,
   PerspectiveCamera,
   WebGLRenderer,
   Clock,
+  AmbientLight,
+  DirectionalLight,
+  TextureLoader,
+  Group,
+  BoxBufferGeometry,
+  ConeBufferGeometry,
+  SphereBufferGeometry,
+  PointLight,
+  Fog,
+  Float32BufferAttribute,
+  RepeatWrapping,
+  PCFSoftShadowMap,
 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
 
-const debug = new dat.GUI();
+const gui = new dat.GUI();
 
 /**
  ** Scene
  */
 const scene = new Scene();
+// Fog
+const fog = new Fog("#262837", 1, 15);
+scene.fog = fog;
+
+/**
+ ** Textures
+ */
+const textureLoader = new TextureLoader();
+
+const doorColorTexture = textureLoader.load("/public/assets/textures/door/color.jpg");
+const doorAlphaTexture = textureLoader.load("/public/assets/textures/door/alpha.jpg");
+const doorAmbientOcclusionTexture = textureLoader.load(
+  "/public/assets/textures/door/ambientOcclusion.jpg"
+);
+const doorHeightTexture = textureLoader.load("/public/assets/textures/door/height.jpg");
+const doorMetalnessTexture = textureLoader.load("/public/assets/textures/door/metalness.jpg");
+const doorNormalTexture = textureLoader.load("/public/assets/textures/door/normal.jpg");
+const doorRoughnessTexture = textureLoader.load("/public/assets/textures/door/roughness.jpg");
+
+const bricksColorTexture = textureLoader.load("/public/assets/textures/bricks/color.jpg");
+const bricksAmbientOcclusionTexture = textureLoader.load(
+  "/public/assets/textures/bricks/ambientOcclusion.jpg"
+);
+const bricksNormalTexture = textureLoader.load("/public/assets/textures/bricks/normal.jpg");
+const bricksRoughnessTexture = textureLoader.load("/public/assets/textures/bricks/roughness.jpg");
+
+const grassColorTexture = textureLoader.load("/public/assets/textures/grass/color.jpg");
+const grassAmbientOcclusionTexture = textureLoader.load(
+  "/public/assets/textures/grass/ambientOcclusion.jpg"
+);
+const grassNormalTexture = textureLoader.load("/public/assets/textures/grass/normal.jpg");
+const grassRoughnessTexture = textureLoader.load("/public/assets/textures/grass/roughness.jpg");
+grassColorTexture.repeat.set(8, 8);
+grassAmbientOcclusionTexture.repeat.set(8, 8);
+grassNormalTexture.repeat.set(8, 8);
+grassRoughnessTexture.repeat.set(8, 8);
+
+grassColorTexture.wrapS = RepeatWrapping;
+grassAmbientOcclusionTexture.wrapS = RepeatWrapping;
+grassNormalTexture.wrapS = RepeatWrapping;
+grassRoughnessTexture.wrapS = RepeatWrapping;
+
+grassColorTexture.wrapT = RepeatWrapping;
+grassAmbientOcclusionTexture.wrapT = RepeatWrapping;
+grassNormalTexture.wrapT = RepeatWrapping;
+grassRoughnessTexture.wrapT = RepeatWrapping;
 
 /**
  ** Objects
  */
-const cube = new Mesh(new BoxGeometry(1, 1, 1), new MeshBasicMaterial({ color: 0xff0000 }));
-scene.add(cube);
+/**
+ * House
+ */
+const house = new Group();
+scene.add(house);
+
+// Walls
+const walls = new Mesh(
+  new BoxBufferGeometry(4, 2.5, 4),
+  new MeshStandardMaterial({
+    map: bricksColorTexture,
+    aoMap: bricksAmbientOcclusionTexture,
+    roughnessMap: bricksRoughnessTexture,
+    normalMap: bricksNormalTexture,
+  })
+);
+walls.geometry.setAttribute(
+  "uv2",
+  new Float32BufferAttribute(walls.geometry.attributes.uv.array, 2)
+);
+
+walls.position.y = 2.5 / 2;
+house.add(walls);
+
+// Roof
+const roof = new Mesh(
+  new ConeBufferGeometry(3.5, 1, 4),
+  new MeshStandardMaterial({ color: "#b35f45" })
+);
+roof.position.y = 2.5 + 0.5;
+roof.rotation.y = Math.PI / 4;
+house.add(roof);
+
+// Door
+const door = new Mesh(
+  new PlaneBufferGeometry(2.2, 2.2, 100, 100),
+  new MeshStandardMaterial({
+    map: doorColorTexture,
+    alphaMap: doorAlphaTexture,
+    transparent: true,
+    aoMap: doorAmbientOcclusionTexture,
+    displacementMap: doorHeightTexture,
+    displacementScale: 0.1,
+    normalMap: doorNormalTexture,
+    metalnessMap: doorMetalnessTexture,
+    roughnessMap: doorRoughnessTexture,
+  })
+);
+door.geometry.setAttribute("uv2", new Float32BufferAttribute(door.geometry.attributes.uv.array, 2));
+door.position.y = 1;
+door.position.z = 2 + 0.01;
+house.add(door);
+
+// Bushes
+const bushGeometry = new SphereBufferGeometry(1, 16, 16);
+const bushMaterial = new MeshStandardMaterial({ color: "#89c854" });
+
+const bush1 = new Mesh(bushGeometry, bushMaterial);
+bush1.scale.set(0.5, 0.5, 0.5);
+bush1.position.set(0.8, 0.2, 2.2);
+
+const bush2 = new Mesh(bushGeometry, bushMaterial);
+bush2.scale.set(0.25, 0.25, 0.25);
+bush2.position.set(1.4, 0.1, 2.1);
+
+const bush3 = new Mesh(bushGeometry, bushMaterial);
+bush3.scale.set(0.4, 0.4, 0.4);
+bush3.position.set(-0.8, 0.1, 2.2);
+
+const bush4 = new Mesh(bushGeometry, bushMaterial);
+bush4.scale.set(0.15, 0.15, 0.15);
+bush4.position.set(-1, 0.05, 2.6);
+
+house.add(bush1, bush2, bush3, bush4);
+
+// Graves
+const graves = new Group();
+scene.add(graves);
+
+const graveGeometry = new BoxBufferGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new MeshStandardMaterial({ color: "#b2b6b1" });
+
+for (let i = 0; i < 50; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radius = 3 + Math.random() * 6;
+  const x = Math.sin(angle) * radius;
+  const z = Math.cos(angle) * radius;
+
+  const grave = new Mesh(graveGeometry, graveMaterial);
+
+  grave.position.set(x, 0.3, z);
+  grave.rotation.y = (Math.random() - 0.5) * 0.4;
+  grave.rotation.z = (Math.random() - 0.5) * 0.4;
+  grave.castShadow = true;
+  graves.add(grave);
+}
+
+// Floor
+const floor = new Mesh(
+  new PlaneBufferGeometry(20, 20),
+  new MeshStandardMaterial({
+    map: grassColorTexture,
+    aoMap: grassAmbientOcclusionTexture,
+    normalMap: grassNormalTexture,
+    roughnessMap: grassRoughnessTexture,
+  })
+);
+floor.geometry.setAttribute(
+  "uv2",
+  new Float32BufferAttribute(floor.geometry.attributes.uv.array, 2)
+);
+floor.rotation.x = -Math.PI * 0.5;
+floor.position.y = 0;
+scene.add(floor);
+
+/**
+ ** Lights
+ */
+// Ambient light
+const ambientLight = new AmbientLight("#b9d5ff", 0.12);
+gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
+scene.add(ambientLight);
+
+// Directional light
+const moonLight = new DirectionalLight("#b9d5ff", 0.12);
+moonLight.position.set(4, 5, -2);
+gui.add(moonLight, "intensity").min(0).max(1).step(0.001);
+gui.add(moonLight.position, "x").min(-5).max(5).step(0.001);
+gui.add(moonLight.position, "y").min(-5).max(5).step(0.001);
+gui.add(moonLight.position, "z").min(-5).max(5).step(0.001);
+scene.add(moonLight);
+
+const doorLight = new PointLight("#ff7d46", 1, 7);
+doorLight.position.set(0, 2.2, 2.7);
+house.add(doorLight);
+
+// Ghosts lights
+const ghost1 = new PointLight("#ff00ff", 2, 3);
+scene.add(ghost1);
+
+const ghost2 = new PointLight("#00ffff", 2, 3);
+scene.add(ghost2);
+
+const ghost3 = new PointLight("#ffff00", 2, 3);
+scene.add(ghost3);
 
 /**
  ** Cameras
  */
 const sizes = { width: window.innerWidth, height: window.innerHeight };
 const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.z = 3;
-camera.lookAt(cube.position);
+camera.position.x = 4;
+camera.position.y = 2;
+camera.position.z = 5;
 scene.add(camera);
 
 /**
@@ -39,6 +241,40 @@ const canvas = document.getElementById("webgl");
 const renderer = new WebGLRenderer({
   canvas,
 });
+
+/**
+ ** Shadows
+ */
+renderer.shadowMap.enabled = true;
+moonLight.castShadow = true;
+doorLight.castShadow = true;
+ghost1.castShadow = true;
+ghost2.castShadow = true;
+ghost3.castShadow = true;
+
+walls.castShadow = true;
+bush1.castShadow = true;
+bush2.castShadow = true;
+bush3.castShadow = true;
+bush4.castShadow = true;
+
+floor.receiveShadow = true;
+
+doorLight.shadow.mapSize.width = 256;
+doorLight.shadow.mapSize.height = 256;
+doorLight.shadow.camera.far = 7;
+
+ghost1.shadow.mapSize.width = 256;
+ghost1.shadow.mapSize.height = 256;
+ghost1.shadow.camera.far = 7;
+
+ghost2.shadow.mapSize.width = 256;
+ghost2.shadow.mapSize.height = 256;
+ghost2.shadow.camera.far = 7;
+
+ghost3.shadow.mapSize.width = 256;
+ghost3.shadow.mapSize.height = 256;
+ghost3.shadow.camera.far = 7;
 
 window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
@@ -56,11 +292,28 @@ controls.enableDamping = true;
 
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setClearColor("#262837");
+renderer.shadowMap.type = PCFSoftShadowMap;
 
 const clock = new Clock();
 
 function animate() {
   const elapsedTime = clock.getElapsedTime();
+
+  const ghostAngle = elapsedTime * 0.5;
+  ghost1.position.x = Math.cos(ghostAngle) * 4;
+  ghost1.position.z = Math.sin(ghostAngle) * 4;
+  ghost1.position.y = Math.sin(elapsedTime * 3);
+
+  const ghostAngle2 = -elapsedTime * 0.32;
+  ghost2.position.x = Math.cos(ghostAngle2) * 5;
+  ghost2.position.z = Math.sin(ghostAngle2) * 5;
+  ghost2.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5);
+
+  const ghostAngle3 = -elapsedTime * 0.18;
+  ghost3.position.x = Math.cos(ghostAngle3) * (7 + Math.sin(elapsedTime * 0.32));
+  ghost3.position.z = Math.sin(ghostAngle3) * (7 + Math.sin(elapsedTime * 0.5));
+  ghost3.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5);
 
   controls.update();
   renderer.render(scene, camera);
